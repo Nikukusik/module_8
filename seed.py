ad020a66077186c4cea7633ef6f702b08d866591
Nikukusik <nikukusika@hmail.com>
2024-06-30 20:38:24 +0300

    mod seed.py

--- seed.py
+++ seed.py
@@ -5,21 +5,19 @@ from models import Author, Quote
 
 
 
-def seed_file(file):
+def load_authors(file):
     with open(f"{file}", "r", encoding="utf-8") as fh:
-        res = json.load(fh)
-    return res
+        data = json.load(fh)
+        for author in data:
+            aut = Author(fullname=author.get("fullname"),
+                         born_date=author.get("born_date"),
+                         born_location=author.get("born_location"),
+                         description=author.get("description"))
+            aut.save()
 
 if __name__ == "__main__":
     file = "authors.json"
     file2 = "qoutes.json"
-    author = Author()
-    quote = Quote()
-    for data in seed_file(file):
-        author = Author(fullname=data.get("fullname"), born_date=data.get("born_date"), born_location=data.get("born_location"), description=data.get("description"))
-        author.save()
-        for data2 in seed_file(file2):
-            quote = Quote(tags=data2.get("tags"), quote=data2.get("quote"), author=author)
-            quote.save()
-            continue
+    load_authors(file)
+
 
