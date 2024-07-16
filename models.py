2ed4f2947c779ff6efd1450949bc8f25ddd80564
Nikukusik <nikukusika@hmail.com>
2024-07-16 22:12:09 +0300

    done

--- models.py
+++ models.py
@@ -3,11 +3,11 @@ from mongoengine import *
 
 class Author(Document):
     _id = ObjectIdField()
-    fullname = StringField(max_length=50)
-    born_date = StringField(max_length=50)
-    born_location = StringField(max_length=50)
+    fullname = StringField(max_length=100)
+    born_date = StringField(max_length=100)
+    born_location = StringField(max_length=100)
     description = StringField(max_length=10000)
 class Quote(Document):
     tags = ListField()
     author = ReferenceField(Author)
-    quote = StringField(max_length=150)
+    quote = StringField(max_length=10000)
